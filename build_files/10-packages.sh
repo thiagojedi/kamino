97da26b781df56063c80286976ced867535f8a0a
Thiago 'Jedi' Cerqueira <thiagoa7@gmail.com>
2026-04-21 20:31:46 -0300

    Remove flatpak override

--- build_files/10-packages.sh
+++ build_files/10-packages.sh
@@ -35,14 +35,6 @@ dnf5 -y install \
 	ptyxis \
 	uld
 
-# TODO: remove me on next flatpak release when preinstall landed in Fedora
-dnf5 -y copr enable ublue-os/flatpak-test
-dnf5 -y copr disable ublue-os/flatpak-test
-dnf5 -y --repo=copr:copr.fedorainfracloud.org:ublue-os:flatpak-test swap flatpak flatpak
-dnf5 -y --repo=copr:copr.fedorainfracloud.org:ublue-os:flatpak-test swap flatpak-libs flatpak-libs
-dnf5 -y --repo=copr:copr.fedorainfracloud.org:ublue-os:flatpak-test swap flatpak-session-helper flatpak-session-helper
-dnf5 -y --repo=copr:copr.fedorainfracloud.org:ublue-os:flatpak-test install flatpak-debuginfo flatpak-libs-debuginfo flatpak-session-helper-debuginfo
-
 # Copy system files
 rsync -rvKl /ctx/system_files/shared/ /
 
